225870766481d547bba4699abf90d7d759d4195a
Josh Ip <joshuaip@hotmail.com>
2018-05-16 18:40:59 -0700

    Initial Commit

---
+++ EnhanceQuizStarter/TriviaQuestions.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaQuestions.swift
+//  EnhanceQuizStarter
+//
+//  Created by Josh Ip on 2018-05-16.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+// import Foundation
+
+struct TriviaQuestions {
+let trivia: [[String : String]] = [
+    ["Question": "Only female koalas can whistle", "Answer": "False"],
+    ["Question": "Blue whales are technically whales", "Answer": "True"],
+    ["Question": "Camels are cannibalistic", "Answer": "False"],
+    ["Question": "All ducks are birds", "Answer": "True"]
+]
+}
+
+TriviaQuestions.count
